0a60872ecbe4feaa1a921c34eb11d8d6fe50ca77
anantsharma3728 <65787459+anantsharma3728@users.noreply.github.com>
2020-11-21 18:51:51 +0530

    Adding minimap and terrain rendering
    added a fully functional minimap with a border and rendered the  terrain to avoid overlapping which caused a few glithces previously

---
+++ Assets/minmapscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minmapscript : MonoBehaviour
+{      
+    public Transform Player;
+
+    void LateUpdate()
+    {
+        Vector3 newposition = Player.position;
+        newposition.y = transform.position.y;
+        transform.position = newposition;
+
+
+        transform.rotation = Quaternion.Euler(90f , Player.eulerAngles.y , 0f);
+
+
+    }
+}

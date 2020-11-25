0320a809c2599ee97bdded1399ead9eaabd3a8d1
anantsharma3728 <65787459+anantsharma3728@users.noreply.github.com>
2020-11-26 01:01:13 +0530

    Walls and texture for new terrain
    Added texture/walls to the training terrain and information terrain .
    added weapons,health boost as pickables,
    added minimap iocon for hover robots

---
+++ Assets/TrainingCentermessage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingCentermessage : MonoBehaviour
+{ 
+    public GameObject uiObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag=="Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSeconds");
+
+        }
+    }
+    IEnumerator WaitForSeconds()
+    {
+        yield return new WaitForSeconds(1);
+        uiObject.SetActive(false);
+
+    }
+}

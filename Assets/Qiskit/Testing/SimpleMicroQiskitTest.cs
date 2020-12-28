f8edef7fe28112388f1b17086ee0d41328c7246c
anantsharma3728 <65787459+anantsharma3728@users.noreply.github.com>
2020-12-29 02:58:16 +0530

    Imported all the new Packages
    Quantumblur,Pyhton,micrqiskit

---
+++ Assets/Qiskit/Testing/SimpleMicroQiskitTest.cs
@@ -0,0 +1,77 @@
+﻿using Qiskit;
+using UnityEngine;
+
+public class SimpleMicroQiskitTest : CalculationBase
+{
+
+    public ComplexNumber[] amplitudes;
+
+
+    public QuantumCircuit Circuit;
+
+
+
+    SimulatorBase simulator = new MicroQiskitSimulator();
+
+    public string QiskitString = "";
+
+    public SimulatorType UsedSimulator;
+
+
+    public void InitTest()
+    {
+        Circuit = new QuantumCircuit(Circuit.NumberOfQubits, Circuit.NumberOfOutputs);
+    }
+    
+
+
+
+    public void StartTest()
+    {
+        amplitudes = simulator.Simulate(Circuit);
+        Probabilities = simulator.GetProbabilities(amplitudes);
+        QiskitString = Circuit.GetQiskitString();
+
+    }
+
+    public void Normalize()
+    {
+        Circuit.Normalize();
+    }
+
+    public override void Calculate()
+    {
+
+        amplitudes = simulator.Simulate(Circuit);
+        Probabilities = simulator.GetProbabilities(amplitudes);
+        QubitCount = Circuit.NumberOfQubits;
+        base.Calculate();
+    }
+
+
+    public void SetSimulator()
+    {
+        switch (UsedSimulator)
+        {
+            case SimulatorType.Micro:
+                simulator = new MicroQiskitSimulator();
+                break;
+            case SimulatorType.PythonMicro:
+                simulator = new PythonMicroQiskitSimulator();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public enum SimulatorType
+    {
+        Micro,
+        PythonMicro
+    }
+}
+
+public class PythonMicroQiskitSimulator : SimulatorBase
+{
+    //You can write your own Simulator
+}

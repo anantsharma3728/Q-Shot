906f31b1f0ee5302d7284030731c551282e45b3f
anantsharma3728 <65787459+anantsharma3728@users.noreply.github.com>
2020-12-29 20:10:45 +0530

    scripting Inventory
    HAlf sript done

---
+++ Assets/InventoryUI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InventoryUI : MonoBehaviour
+{ 
+    Inventory inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = Inventory.instance;
+        inventory.onItemChangeCallback += UpdateUI;
+
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+
+        InventorySlot[] slots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Inventory"))
+        {
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+        }
+    }
+  
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory.items.Count)  // If there is an item to add
+            {
+                slots[i].AddItem(inventory.items[i]);   // Add it
+            }
+            else
+            {
+                // Otherwise clear the slot
+                slots[i].ClearSlot();
+            }
+        };
+
+    }
+}

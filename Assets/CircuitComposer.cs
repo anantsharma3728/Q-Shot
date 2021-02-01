f87862872716cfdeb3c810b34860af461d802052
anantsharma3728 <65787459+anantsharma3728@users.noreply.github.com>
2021-02-01 11:08:53 +0530

    CircuitComposer slot.

---
+++ Assets/CircuitComposer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircuitComposer : MonoBehaviour
+{
+    public static bool isCircuitON= false;
+
+    [SerializeField] GameObject circuit;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+              if (isCircuitON)
+              {
+                  CircuitOff();
+              }
+              else
+              {
+                  CircuitOn();
+              }
+        }
+    }
+}
+void CircuitOn()
+{
+
+}
+void CircuitOff()
+{
+    circuit.SetActive(true);
+    
+}

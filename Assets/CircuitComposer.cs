ad02d6b6ae99b95b50e88afbca9469a81137c87e
anantsharma3728 <65787459+anantsharma3728@users.noreply.github.com>
2021-02-14 18:24:27 +0530

    CircuitComposer
    Circuit composer UI ready ,code left

---
+++ Assets/CircuitComposer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class CircuitComposer : MonoBehaviour
+{
+     public GameObject ComposerRoot;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    // Lock cursor when clicking outside of menu
+       
+        if (!ComposerRoot.activeSelf && Input.GetMouseButtonDown(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        if(Input.GetKeyDown(KeyCode.Escape) | Input.GetKeyDown(KeyCode.RightShift))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        if(Input.GetKeyDown(KeyCode.RightShift))
+        {
+             ComposerRoot.SetActive(true);
+        }
+
+    }
+}

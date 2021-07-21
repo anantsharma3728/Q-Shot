8eabff3838143f69976ba774c86a0b85cd0a6087
anantsharma3728 <anantsharma2410@gmail.com>
2021-07-22 00:52:25 +0530

    Editing to mesh

--- Assets/CircuitComposer.cs
+++ Assets/CircuitComposer.cs
@@ -33,6 +33,10 @@ public class CircuitComposer : MonoBehaviour
         {
              ComposerRoot.SetActive(true);
         }
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+             ComposerRoot.SetActive(false);
+        }
 
     }
 }
